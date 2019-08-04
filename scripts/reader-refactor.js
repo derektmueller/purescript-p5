32291d9cce4f576ce7a43faf7e06163ceb9be966
Derek Mueller <dtmuelle@gmail.com>
2019-08-04 11:32:44 -0700

    add script to perform reader refactor

---
+++ scripts/reader-refactor.js
@@ -0,0 +1,45 @@
+
+const fs = require('fs');
+const filename = process.argv[2];
+
+let contents = fs.readFileSync(filename).toString();
+
+const effects = contents
+  .split('\n')
+  .filter(l => l.match(/=|::/))
+  .filter(l => !l.match(/foreign/))
+  .reduce((acc, l) => {
+    if(acc[acc.length - 1].length < 2) {
+      acc[acc.length - 1].push(l);
+    } else {
+      acc.push([l]);
+    }
+
+    return acc;
+  }, [[]])
+  .filter(([signature, body]) => 
+    signature.match(/Effect Unit\)$/))
+  .map(([signature, body]) => {
+    const fnName = signature.match(/(\w+) ::/)[1];
+
+    return [
+      fnName,
+      new RegExp(
+        signature.replace(/\(/g, '\\(').replace(/\)/g, '\\)'), 'g'),
+      new RegExp(
+        body.replace(/\(/g, '\\(').replace(/\)/g, '\\)'), 'g'),
+      signature
+        .replace(/\(Effect Unit\)$/, 'P5M Unit')
+        .replace(/:: P5 ->/, '::'),
+      body
+        .replace(new RegExp(`${fnName} p5`), fnName)
+        .replace(/= /, '= do \n  p5 <- ask\n  ')
+    ];
+  });
+
+effects.forEach(([, signature, body, newSignature, newBody]) => {
+  contents = contents.replace(signature, newSignature);
+  contents = contents.replace(body, newBody);
+});
+
+console.log(contents);
